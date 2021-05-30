4d194db075da0f9cd1e38d3df640dba1172a9497
Joao Marcos Costa <jmcosta944@gmail.com>
2021-05-30 12:19:28 -0300

    Add comments to hello.s

--- hello.s
+++ hello.s
@@ -1,6 +1,10 @@
 section .data
 	text db "Hello, World!", 10
 
+; text: name of memory address
+; db : define bytes
+; 10 : '\n'
+
 section .text
 	global _start
 
@@ -8,8 +12,14 @@ _start:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, text
-	mov rdx, 14
+	mov rdx, 13
 	syscall
+; syscall: when a program requests a service from the kernel
+; syscalls have an ID and take arguments
+; write "hello world\n" -> sys_write
+; sys_write(file_descriptor, buffer, count)
+; ID = 1, file_descriptor = 1 (stdout), buffer=text, count=14
+; ID = rax, file_descriptor = rdi, buffer = rsi, count = rdx
 
 	mov rax, 60
 	mov rdi, 0
